23672b209d08712c9151f2b9962fda39dafa60c5
kimhyeongju <hengzhu1994@gmail.com>
2020-09-28 10:55:06 +0900

    gpiozero

---
+++ gpiozero-test/ex06_distance.py
@@ -0,0 +1,7 @@
+from gpiozero import DistanceSensor
+from time import sleep
+
+sensor = DistanceSensor(23, 24) # echo, trig
+while True:
+    print('Distance to nearest object is', sensor.distance, 'm')
+    sleep(1)
