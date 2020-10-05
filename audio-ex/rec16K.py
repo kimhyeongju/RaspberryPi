c239cb708e7371ad74f7ebc5dc2a7ee81e56b736
kimhyeongju <hengzhu1994@gmail.com>
2020-10-05 13:27:40 +0900

    16k로 변환

---
+++ audio-ex/rec16K.py
@@ -0,0 +1,54 @@
+import pyaudio
+import wave
+import io
+from pydub import AudioSegment
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+SAMPLE_WIDTH = 2
+CHANNELS = 1
+RATE = 44100
+
+def record(record_seconds=5):
+    p = pyaudio.PyAudio()
+    stream = p.open(input_device_index = 0,
+                    format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    print("Start to record the audio.")
+
+    frames = []
+    for i in range(0, int(RATE / CHUNK * record_seconds)):
+        data = stream.read(CHUNK, exception_on_overflow=False)
+        frames.append(data)
+    print("Recording is finished.")
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    return frames
+
+def convertTo16K(file):     # 아래 wavfile이 넘어옴 
+    # 16000 으로 변환
+    sound = AudioSegment.from_file(file)
+    sound = sound.set_frame_rate(16000)     # frame rate를 160000으로 설정
+    rec_data = io.BytesIO()
+    sound.export
+    (rec_data, format="wav")    # rec_data -> 16K wave
+    return rec_data
+
+# wav 파일 저장
+def getWav(frames):
+    wavfile = io.BytesIO()
+    wf = wave.open
+    (wavfile, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(SAMPLE_WIDTH)
+    wf.setframerate(RATE)
+    wf.writeframes
+    (b''.join(frames))
+    wavfile.seek(0)
+    return wavfile
