23e9bb0b8c4cf5527607ea536dd0edc4ff3fb47a
kimhyeongju <hengzhu1994@gmail.com>
2020-10-07 16:14:13 +0900

    updated

--- car-ex/rccar.py
+++ car-ex/rccar.py
@@ -1,4 +1,4 @@
-from btsocket import btsocket
+from btsocket import BtSocket
 from bluetooth import *
 
 from gpiozero import Robot
@@ -24,19 +24,19 @@ def car_control(x,y):
             car.left(sx)
     else:
         car.stop()
-        
+
 
 def control(tokens):
     command = int(tokens[0])
     if command == 0:     # 주행 모드
         x = int(tokens[1])
         y = int(tokens[2])
-    
+        car_control(x,y)
     elif command == 1:  # 카메라 모드
         angle = int(tokens[1])
         servo.angle = angle
 
-client_socket = btsocket( RFCOMM )
+client_socket = BtSocket( RFCOMM )
 client_socket.connect(("00:18:91:D7:A1:D1", 1))
 
 try:
